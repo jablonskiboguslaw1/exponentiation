60eb4728158a1871d67fb966c4ce82c53d2e0abf
jablonskiboguslaw1 <jablonski.boguslaw@gmail.com>
2022-05-29 21:58:57 +0200

    Initial commit

---
+++ src/main/java/com/jablonski/exponentiation/ExponentiationApplication.java
@@ -0,0 +1,13 @@
+package com.jablonski.exponentiation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExponentiationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExponentiationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jablonski/exponentiation/ExponentiationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jablonski.exponentiation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExponentiationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
